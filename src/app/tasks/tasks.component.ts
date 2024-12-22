72eade5ccc55feaae22b190131328591137f1ba5
utsav <ukaran11@gmail.com>
2024-12-23 01:15:52 +0530

    Utsav | Added task component

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
+
+@Component({
+  selector: 'app-tasks',
+  imports: [TaskComponent],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css',
+})
+export class TasksComponent {
+  @Input() name?: string;
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,6 +7,12 @@ import {
   output,
   Output,
 } from '@angular/core';
+
+type User = {
+  id: string;
+  avatar: string;
+  name: string;
+};
 @Component({
   selector: 'app-user',
   imports: [],
@@ -14,17 +20,18 @@ import {
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: User;
   @Output() select = new EventEmitter<string>();
   // select = output<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 
   // avatar = input.required<string>();
